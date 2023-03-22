4c879942c1371693ed9266f2cf362700485fe29f
GitNicksHub <aitmatov.82@mail.ru>
2023-03-22 17:26:30 -0400

    first commit

---
+++ Cocktail/main.js
@@ -0,0 +1,56 @@
+import { data } from './getData.js'
+
+const container = document.querySelector('.container');
+const searchBtn = document.getElementsByTagName('button');
+const input = document.querySelector('input')
+const drinksData = data();
+
+function showDrinksOnUI(arr) {
+    for (let drink of arr) {
+        const div = document.createElement('div');
+        const img = document.createElement('img');
+        const drinkName = document.createElement('h3');
+        img.src = drink.strDrinkThumb;
+        drinkName.innerHTML = drink.strDrink;
+        div.appendChild(drinkName);
+        div.appendChild(img);
+        container.appendChild(div);
+    }
+}
+
+function searchFilter(inputVal) {
+    const filteredDrinks = [];
+    for (let el of drinksData) {
+        let nameOfDrinks = el.strDrink.toLowerCase()
+        if (nameOfDrinks.includes(inputVal.toLowerCase())) {
+            filteredDrinks.push(el)
+        }
+    }
+    return filteredDrinks;
+}
+
+function clear() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+}
+
+function clickEvent() {
+    if (input.value === '') {
+        // console.log('input value is empty')
+        clear()
+        showDrinksOnUI(drinksData)
+    } else {
+        const inputValue = input.value
+        const drinks = searchFilter(inputValue)
+        console.log(drinks, '<<<drinks')
+        clear()
+        showDrinksOnUI(drinks)
+    }
+}
+
+// input field get triggered
+input.addEventListener('keyup', clickEvent)
+
+
+showDrinksOnUI(drinksData);
---
+++ cocktailMenue.js
@@ -0,0 +1,5 @@
+
+import { data } from "./Cocktail/getData.js";
+
+const drinksData = data();
+console.log(drinksData, "<<< This is my imported data");
